9ddf964e2792f2b8bc5c115074f5596a2549bdca
it27472 <istiak.tushar.qtec@gmail.com>
2022-03-03 18:52:40 +0600

    product details

---
+++ assets/js/sync-slider.js
@@ -0,0 +1,32 @@
+var galleryThumbs = new Swiper(".gallery-navs", {
+    centeredSlides: true,
+    centeredSlidesBounds: true,
+    slidesPerView: 4,
+    watchOverflow: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    direction: 'vertical'
+});
+
+var galleryMain = new Swiper(".gallery-main", {
+    watchOverflow: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    // slidesPerView: 1,
+    preventInteractionOnTransition: true,
+    effect: 'fade',
+        fadeEffect: {
+        crossFade: true
+    },
+    thumbs: {
+        swiper: galleryThumbs
+    }
+});
+
+galleryMain.on('slideChangeTransitionStart', function() {
+    galleryThumbs.slideTo(galleryMain.activeIndex);
+});
+
+galleryThumbs.on('transitionStart', function(){
+    galleryMain.slideTo(galleryThumbs.activeIndex);
+});
